e62f6dece596bd3c8c247e4e53a1353ca87a4265
Primiano Tucci <p.tucci@gmail.com>
2014-10-10 21:28:39 +0200

    Add build/install_build_deps.sh

---
+++ install_build_deps.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+sudo apt-get update -qq
+sudo apt-get install git gnupg flex bison gperf build-essential zip curl libc6-dev libncurses-dev x11proto-core-dev libreadline-dev g++-multilib mingw32 tofrodos python-markdown libxml2-utils xsltproc zlib1g-dev libc6-i386 lib32stdc++6 lib32gcc1 lib32ncurses5 lib32z1 ia32-libs file psmisc uuid-dev liblzo2-dev
+
